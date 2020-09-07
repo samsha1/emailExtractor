87a96ef3b3a86c6858d33d6135f2718161be062f
samrat <samratshakya5@gmail.com>
2020-09-07 22:41:23 +0545

    setup and app customization

---
+++ src/ElevateAppBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import AddInput from "./components/AddInput";
+
+export default function ElevateAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">Advanced Email Sorter V1</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container>
+        <Box my={2}>
+          <AddInput />
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ src/components/AddInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddInput extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Input Begins here..</h1>
+      </div>
+    );
+  }
+}
+
+export default AddInput;
