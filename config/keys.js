7ac5536d216b137f3b07cc0b962da603ba691d98
samrat <samratshakya5@gmail.com>
2020-12-08 08:32:14 +0545

    --

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://devconnector:connect123@samrat.lcvru.mongodb.net/emailextractor?retryWrites=true&w=majority",
-  // "mongodb+srv://scrapingmesh:connect123627@appscrapingmesh.rq3f6.mongodb.net/appscrapingmesh?retryWrites=true&w=majority",
+  // "mongodb+srv://scrapingmesh:connect123627@appscrapingmesh.rq3f6.mongodb.net/appscrapingmesh?retryWrites=true&w=majority", //
   secretOrKey: "secret",
 };
